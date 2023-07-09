c4f09ba0fc2cbc935ff93045a959224ff95f3f3a
BinSchoolApp <136219480+binschoolapp@users.noreply.github.com>
2023-07-09 22:03:49 +0800

    init

--- 202-integer/ValueTypesDefault.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract ValueTypesDefault{
-  int public value_int; // value_int = 0
-  int8 public value_int8; // value_int8 = 0
-  uint public value_uint; // value_uint = 0
-  uint16 public value_uint16; // value_uint16 = 0
-}
--- 202-integer/ValueTypesError.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract ValueTypesError {
-  uint public a = 2;
-  uint public b = 3;
-  uint public a_sub_b = a - b; 
-}
